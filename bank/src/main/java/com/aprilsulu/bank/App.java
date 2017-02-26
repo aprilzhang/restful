ff1c8599d42fdac17bd210c75c4e3d4e27f11627
aprilzhang <aprilsulu@gmail.com>
2017-02-26 23:12:39 +0000

    Tutorial

---
+++ bank/src/main/java/com/aprilsulu/bank/App.java
@@ -0,0 +1,13 @@
+package com.aprilsulu.bank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
